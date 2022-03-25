5f1f89bfe668945ef5f18d49267ad2a6b65244d7
Feroze Khan <87601912+ferozemewati@users.noreply.github.com>
2022-03-25 18:42:57 +0530

    FactorialFind By Myself
    In This We Find Simple Factorial and Find Factorial By User Input With The Help Of For Loop or While Loop.

---
+++ Factorial.java
@@ -0,0 +1,72 @@
+//Factorial
+
+/*
+public class Factorial {
+    public static void main(String[] args) {
+        int fact=1, i=1;
+        while (i<=5){
+            fact=fact*i;
+            i++;
+        }
+        System.out.print(fact);
+    }
+}
+*/
+
+
+//Factorial By User Input With The Help Of For Loop.
+
+/*
+import java.util.Scanner;
+
+class fact{
+    void fn(){
+        int factorial=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++){
+            factorial=factorial*i;
+        }
+        System.out.println(factorial);
+    }
+}
+public class Factorial {
+    public static void main(String[] args) {
+        fact obj=new fact();
+        obj.fn();
+    }
+}
+*/
+
+
+//Factorial By User Input With The Help Of While Loop.
+
+/*
+import java.util.Scanner;
+
+class fact{
+    void fn(){
+        int i=1;
+        int factorial=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n= sc.nextInt();
+        while (i<=n){
+            factorial=factorial*i;
+            i++;
+        }
+        System.out.print(factorial);
+    }
+}
+public class Factorial {
+    public static void main(String[] args) {
+        fact obj=new fact();
+        obj.fn();
+    }
+}
+*/
+
+
+
+
